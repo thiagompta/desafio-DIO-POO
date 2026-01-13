a152a587a1511fb118c46d4761a3d6a7e322ae99
Thiago Silva <thiagompta008@hotmail.com>
2026-01-12 22:23:18 -0300

    Corrigindo alguns problemas e apagando esses arquivo, também criei a classe Main, Curso e Mentoria

---
+++ Desafio_java/src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+        Curso curso = new Curso();
+        curso.setTitulo("Desafio Bootcamp Java");
+        curso.setDescricao("Descrição do Bootcamp");
+        curso.setCargaHoraria(8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Ex Bootcamp Java");
+        curso2.setDescricao("Descrição do ex. 2");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria do Bootcamp java ");
+        mentoria.setDescricao("Essa é onde fica a descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+
+    }
+}
