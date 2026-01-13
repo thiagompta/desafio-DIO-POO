bacd85a2aaf895809db1b83c3bb7affdf7dd6555
Thiago Silva <thiagompta008@hotmail.com>
2026-01-13 15:08:58 -0300

    Criando a classe conteudo para ser mae das outras classes

--- Desafio_java/src/Main.java
+++ Desafio_java/src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -9,12 +10,14 @@ public class Main {
         curso.setTitulo("Desafio Bootcamp Java");
         curso.setDescricao("Descrição do Bootcamp");
         curso.setCargaHoraria(8);
+        
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso Ex Bootcamp Java");
         curso2.setDescricao("Descrição do ex. 2");
         curso2.setCargaHoraria(4);
         System.out.println(curso);
         System.out.println(curso2);
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria do Bootcamp java ");
